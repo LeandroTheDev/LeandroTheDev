729f5c45409a4976f98aedb7d645e2e3e9c259da
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-02-14 19:40:23 -0300

    Drive Authentication Default

--- website_server/components/drive/credentials.js
+++ website_server/components/drive/credentials.js
@@ -1,5 +1,5 @@
 class Credentials {
-    static username = "therooter";
-    static password = "202001";
+    static username = "admin";
+    static password = "123";
 }
 module.exports = Credentials;
